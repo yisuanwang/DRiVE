7aeceed0f9c3e969c724129b297a77e1cf4a56c7
mingzesun <88081374+mingzesun0514@users.noreply.github.com>
2025-06-12 11:45:33 +0800

    Add files via upload

---
+++ rigging/example_configs/infer_joints.py
@@ -0,0 +1,90 @@
+import os
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+from PIL import Image
+from tqdm import tqdm
+
+import gecco_torch
+from gecco_torch.diffusion import Diffusion
+from gecco_torch.structs import Example
+from gecco_torch.utils import save_pcd_as_ply, symmetric_point_cloud_torch, remove_points_near_origin, sort_point_cloud
+
+
+
+def sample_joints(config_root, config_name, ckpt_name, joints_num):
+    config = gecco_torch.load_config(f'{config_root}/{config_name}') # load the model definition
+    model: Diffusion = config.model
+
+    state_dict = torch.load(f'{config_root}/{ckpt_name}', map_location='cpu')
+
+    model.load_state_dict(state_dict['ema_state_dict'])
+    model = model.eval()
+
+    data = config.data
+    data.setup() # PyTorch lightning data modules need to be setup before use
+    batch: Example = next(iter(data.val_dataloader()))
+    print(batch) # print the batch to see what it contains
+
+    # find out the best backend
+    if torch.cuda.is_available():
+        map_device = lambda x: x.to(device='cuda')
+    else:
+        map_device = lambda x: x
+
+    model: Diffusion = map_device(model).eval()
+    context = batch.ctx.apply_to_tensors(map_device)
+
+    with torch.no_grad():
+        with torch.autocast('cuda', dtype=torch.float16):
+            samples = model.sample_stochastic(
+                            (6, joints_num, 3),
+                            context=context,
+                            with_pbar=True,
+                        )
+
+    return samples, batch, context
+        
+
+if __name__ == "__main__":   
+    to_pil_image = transforms.ToPILImage()            
+    config_root = '/mnt/workspace/project/LGM_siga/Real2Character/pcd_diff/gecco/gecco-torch/example_configs/'
+    config_name = 'train_joints.py'
+    common_ckpt_path = 'lightning_logs/common_joints_global_3d/checkpoints/epoch=147-val_loss=51.811.ckpt'
+    special_ckpt_path = 'lightning_logs/special_joints_global_3d/checkpoints/epoch=148-val_loss=63.883.ckpt'
+    # common_ckpt_path = 'lightning_logs/common_joints_cross_att_3d_2d/checkpoints/epoch=148-val_loss=49.486.ckpt'
+    # special_ckpt_path = 'lightning_logs/special_joints_cross_att_3d_2d/checkpoints/epoch=168-val_loss=61.023.ckpt'
+    # common_ckpt_path = 'lightning_logs/common_joints_cross_att_3d_2d_large/checkpoints/epoch=196-val_loss=46.857.ckpt'
+    # special_ckpt_path = 'lightning_logs/special_joints_cross_att_3d_2d_large/checkpoints/epoch=174-val_loss=36.594.ckpt'
+    
+    common_samples, batch, context = sample_joints(config_root, config_name, common_ckpt_path, 25)
+    special_samples, _, _ = sample_joints(config_root, config_name, special_ckpt_path, 75)
+    
+    # Save
+    for i in tqdm(range(common_samples.shape[0])):
+        image = context.image[i]
+        gaussian_pcd = context.gaussian_pcd[i]
+        shape_name = int(context.shape_name[i].detach().cpu().numpy())
+
+        common_pcd_sample = symmetric_point_cloud_torch(common_samples[i]).float()
+        common_pcd_sample = sort_point_cloud(common_pcd_sample)
+        
+        special_pcd_sample = special_samples[i].float()
+        special_pcd_sample = symmetric_point_cloud_torch(special_pcd_sample)
+        # try:
+        #     special_pcd_sample = symmetric_point_cloud_torch(special_pcd_sample)
+        # except:
+        #     special_pcd_sample = special_pcd_sample
+        special_pcd_sample = sort_point_cloud(special_pcd_sample)
+        
+        special_pcd_sample, mask = remove_points_near_origin(special_pcd_sample)
+        
+        pcd_sample = torch.cat([common_pcd_sample, special_pcd_sample], dim=0)
+
+        save_pcd_as_ply(pcd_sample.detach().cpu().numpy(), os.path.join(config_root, 'results', 'ablation/RealHuman/joints_pcd/{:d}.ply'.format(shape_name)))
+        save_pcd_as_ply(common_pcd_sample.detach().cpu().numpy(), os.path.join(config_root, 'results', 'ablation/RealHuman/joints_pcd/{:d}_common.ply'.format(shape_name)))
+        save_pcd_as_ply(gaussian_pcd.detach().cpu().numpy(), os.path.join(config_root, 'results', 'ablation/RealHuman/gaussian_pcd/{:d}.ply'.format(shape_name)))
+
+
+
---
+++ rigging/example_configs/train_joints.py
@@ -0,0 +1,114 @@
+import os
+
+import torch
+import lightning.pytorch as pl
+
+from gecco_torch.diffusion import EDMPrecond, Diffusion
+from gecco_torch.reparam import UVLReparam, GaussianReparam
+from gecco_torch.diffusion import Diffusion, LogUniformSchedule, EDMLoss
+from gecco_torch.models.set_transformer_new import SetTransformer
+from gecco_torch.models.feature_pyramid import ConvNeXtExtractor
+from gecco_torch.models.ray_new import RayNetwork
+from gecco_torch.models.activation import GaussianActivation
+from gecco_torch.vis import PCVisCallback
+# from gecco_torch.data.skeleton_cond import TaskonomyDataModule
+# from gecco_torch.data.skeleton_cond_mesh import TaskonomyDataModule
+from gecco_torch.data.skeleton_test import TaskonomyDataModule
+from gecco_torch.ema import EMACallback
+
+
+
+# train_dataset_path = "/mnt/workspace/project/data/vrm/test_3dgs/"
+# test_dataset_path = "/mnt/workspace/project/data/vrm/test_3dgs/"
+# rgb_dataset_path = "/mnt/workspace/project/data/vrm/rendered_images_4view/"
+train_dataset_path = "/mnt/workspace/project/data/vrm/test_3dgs_demo/"
+test_dataset_path = "/mnt/workspace/project/data/vrm/test_3dgs_demo/"
+rgb_dataset_path = "/mnt/workspace/project/data/vrm/rendered_images_4view/"
+
+
+NUM_STEPS = 2_000_000
+SAVE_EVERY = 10_000
+BATCH_SIZE = 64 # 48, 64, 16, 36
+epoch_size = None
+num_epochs = NUM_STEPS // SAVE_EVERY
+print(
+    f"num steps: {NUM_STEPS}, batch size: {BATCH_SIZE}, save_every: {SAVE_EVERY}, epoch size: {epoch_size}, num epochs: {num_epochs}"
+)
+
+# Reparametrization makes the point cloud more isotropic.
+# The values below are computed with the notebook in notebooks/compute_hyperparams.ipynb
+reparam = GaussianReparam(
+    mean=torch.tensor([-0.0006006215699017048, 0.08998294919729233, 0.017616869881749153]),
+    sigma=torch.tensor([0.17428095638751984, 0.28580644726753235, 0.049740005284547806]),
+)
+
+# Set up the network. RayNetwork is responsible for augmenting cloud features with local
+# features extracted from the context 2d image.
+network = RayNetwork(
+    backbone=SetTransformer(  # a point cloud network with extra "global" input for the `t` parameter
+        n_layers=6,
+        num_inducers=64,
+        feature_dim=3 * 128, #3 * 128
+        t_embed_dim=1,  # dimensionality of the `t` parameter
+        num_heads=8,
+        activation=GaussianActivation,
+    ),
+    reparam=reparam,  # we need the reparam object to go between data and diffusion spaces for ray lookup
+    context_dims=(96, 192, 384),  # feature dimensions at scale 1, 2, 4
+)
+
+# Set up the diffusion model. This is largely agnostic of the
+# 3d point cloud task and could be used for 2d image diffusion as well.
+model = Diffusion(
+    backbone=EDMPrecond(
+        model=network,
+    ),
+    conditioner=ConvNeXtExtractor(),
+    reparam=reparam,
+    loss=EDMLoss(
+        schedule=LogUniformSchedule(
+            max=180.0,
+        ),
+    ),
+)
+
+# set up the data module
+data = TaskonomyDataModule(
+    train_dataset_path,
+    test_dataset_path,
+    rgb_dataset_path,
+    n_points=200,
+    batch_size=BATCH_SIZE,
+    num_workers=16,
+    epoch_size=epoch_size,
+    val_size=64, #184, 36
+    # input_type='pcd',
+)
+
+
+def trainer():
+    return pl.Trainer(
+        default_root_dir=os.path.split(__file__)[0],
+        callbacks=[
+            EMACallback(decay=0.999),
+            pl.callbacks.ModelCheckpoint(),
+            pl.callbacks.ModelCheckpoint(
+                monitor="val_loss",
+                filename="{epoch}-{val_loss:.3f}",
+                save_top_k=1,
+                mode="min",
+            ),
+            PCVisCallback(
+                n=8, n_steps=128, point_size=0.05
+            ),
+        ],
+        max_epochs=num_epochs,
+        precision="16-mixed",
+        gradient_clip_val=1.0,
+        gradient_clip_algorithm="norm",
+        strategy='ddp_find_unused_parameters_true', 
+    )
+
+
+if __name__ == "__main__":
+    trainer().fit(model, data)
